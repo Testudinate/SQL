2db82f8fef241dd5c84658f694d6c8fecc561052
Developer <klevtsovstanislav@gmial.com>
2015-11-14 03:22:26 +0300

    Update 02_script.sql

--- _-_PL-SQL/02_script.sql
+++ _-_PL-SQL/02_script.sql
@@ -1,3 +1,10 @@
-2.Выбрать всех работников, учившихся в Краснодаре.
+2.Выбрать всех работников, учившихся в Москве.
+
+select r.fio
+  from RABOT as r
+  join EDUCATION as e on e.idr = r.idr
+  join VUZ as v on v.idv = e.idv
+               and v.city = 'Москва'
+               
 
 
