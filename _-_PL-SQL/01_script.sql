e058d6dbd1d28601ac027cd25362c43d29fa4d9c
Developer <klevtsovstanislav@gmial.com>
2015-11-14 04:23:00 +0300

    Update 01_script.sql

--- _-_PL-SQL/01_script.sql
+++ _-_PL-SQL/01_script.sql
@@ -1,7 +1,7 @@
-1.Выбрать всех работников из цеха с названием РУД.
+1.Выбрать всех работников из цеха с названием PP1.
 
 select r.fio
-  from RABOT as r
-  join CEH as c on c.idc = r.idc
-               and c.name = 'РУД'
+  from RABOT r
+  join CEH c on c.idc = r.idc
+               and c.name_ceh = 'PP1'
                
