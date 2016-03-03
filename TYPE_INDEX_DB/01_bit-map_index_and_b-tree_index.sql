080e9490723eb5ba21d1fc29b53323aa223c4031
Developer <klevtsovstanislav@gmial.com>
2016-03-03 15:18:10 +0300

    Update 01_bit-map_index_and_b-tree_index.sql

--- TYPE_INDEX_DB/01_bit-map_index_and_b-tree_index.sql
+++ TYPE_INDEX_DB/01_bit-map_index_and_b-tree_index.sql
@@ -3,15 +3,16 @@
 http://citforum.ru/database/oracle/bb_indexes/
 http://citforum.ru/database/oracle/bitmap_index/
 
-Анализ и часть текста и статьи:
+I Анализ и часть текста и статьи:
 
-Create table unilk.test_normal (empno number(10), ename varchar2(30), sal number(10));
+1. Create table test_normal (empno number(10), ename varchar2(30), sal number(10));
 
+2. 
 declare 
 Begin
 For i in 1..1000000
 Loop
-   Insert into unilk.test_normal 
+   Insert into test_normal 
    values(i, dbms_random.string('U',30), dbms_random.value(1000,7000));
    If mod(i, 10000) = 0 then
    Commit;
@@ -19,3 +20,6 @@ Loop
 End loop;
 End;
 /
+
+3. Create table test_random as select /*+ append */ * from test_normal order by dbms_random.random;
+
