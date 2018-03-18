f256428a728d242a36754b6c0be91ab5981131af
Developer <klevtsovstanislav@gmial.com>
2018-03-18 22:26:44 +0300

    Create 28_loc_&_iloc.py

---
+++ 28_loc_&_iloc.py
@@ -0,0 +1,30 @@
+loc and iloc (3)
+
+It's also possible to select only columns with loc and iloc. In both cases, you simply put a slice going from beginning to end in front of the comma:
+
+cars.loc[:, 'country']
+cars.iloc[:, 1]
+
+cars.loc[:, ['country','drives_right']]
+cars.iloc[:, [1, 2]]
+
+Instructions
+100xp
+
+    Print out the drives_right column as a Series using loc or iloc.
+    Print out the drives_right column as a DataFrame using loc or iloc.
+    Print out both the cars_per_cap and drives_right column as a DataFrame using loc or 
+    
+    
+    # Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Print out drives_right column as Series
+print(cars.iloc[:,2])
+
+# Print out drives_right column as DataFrame
+print(cars.iloc[:,[2]])
+
+# Print out cars_per_cap and drives_right as DataFrame
+print(cars.iloc[:,[0,2]])
